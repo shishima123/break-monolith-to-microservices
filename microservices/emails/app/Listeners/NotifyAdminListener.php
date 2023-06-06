1970c5521f6ccbfe18bd769df7ba6f156aeac682
Phuoc Nguyen <shishima21@gmail.com>
2023-06-07 00:58:57 +0700

    init email services

---
+++ microservices/emails/app/Listeners/NotifyAdminListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Message;
+use Illuminate\Queue\InteractsWithQueue;
+
+class NotifyAdminListener
+{
+//    public function handle(OrderCompletedEvent $event)
+//    {
+//        $order = $event->order;
+//
+//        \Mail::send('admin', ['order' => $order], function (Message $message) {
+//            $message->subject('An Order has been completed');
+//            $message->to('admin@admin.com');
+//        });
+//    }
+}
---
+++ microservices/emails/app/Listeners/NotifyAmbassadorListener.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Message;
+use Illuminate\Queue\InteractsWithQueue;
+
+class NotifyAmbassadorListener
+{
+//    public function handle(OrderCompletedEvent $event)
+//    {
+//        $order = $event->order;
+//
+//        \Mail::send('ambassador', ['order' => $order], function (Message $message) use ($order) {
+//            $message->subject('An Order has been completed');
+//            $message->to($order->ambassador_email);
+//        });
+//    }
+}
